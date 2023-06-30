1bf9b810eefc9f09526b46f9235df4193884f41f
learntheropes <giovanni@learntheropes.xyz>
2023-06-30 16:02:53 +0200

    assign cyclic_url env var to deploymentDomain

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   ],
 
   auth: {
-    origin: deploymentDomain,
+    origin: 'https://outrageous-ox-hat.cyclic.app', // deploymentDomain,
     globalAppMiddleware: true
   }
 });
