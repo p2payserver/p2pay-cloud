77de3184302e65efc16c0f911968d99a327b4935
learntheropes <giovanni@learntheropes.xyz>
2023-07-07 05:43:49 +0200

    fix favicon title and description

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,21 @@
+<script setup>
+const i18nHead = useLocaleHead({});
+
+useHead({
+  htmlAttrs: {
+    lang: (i18nHead) ? i18nHead.value.htmlAttrs.lang : null
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])]
+});
+
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+  addSeoAttributes: true
+});
+</script>
+
 <template>
   <div>
     <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
@@ -21,24 +39,6 @@
     </Html>
   </div>
 </template>
-
-<script setup>
-const i18nHead = useLocaleHead({});
-
-useHead({
-  htmlAttrs: {
-    lang: (i18nHead) ? i18nHead.value.htmlAttrs.lang : null
-  },
-  link: [...(i18nHead.value.link || [])],
-  meta: [...(i18nHead.value.meta || [])]
-});
-
-const head = useLocaleHead({
-  addDirAttribute: true,
-  identifierAttribute: 'id',
-  addSeoAttributes: true
-});
-</script>
 
 <style scoped>
 @media screen and (max-width: 768px) {
