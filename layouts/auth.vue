c02e784cd4f93555d61c10c4edebc5b346441294
learntheropes <giovanni@learntheropes.xyz>
2023-07-11 03:52:26 +0200

    improve auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <LayoutAuthNavbar />
     <div class="hero is-fullheight">
+      <div class="hero-head">
+        <LayoutAuthNavbar />
+      </div>
       <div class="hero-body is-justify-content-center">
         <div class="columns is-centered">
           <div class="column is-two-third is-justify-content-center">
